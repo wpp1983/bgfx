e7566e46c0b569b59560694a7ded01bfc15ac0d9
wangpeng <wangpeng100842@pwrd.com>
2023-08-04 11:38:08 +0800

    Android 读取文件改为使用 assetManager 读取包内文件

--- examples/common/entry/entry.h
+++ examples/common/entry/entry.h
@@ -42,329 +42,334 @@ extern "C" int _main_(int _argc, char** _argv);
 
 namespace entry
 {
-	ENTRY_HANDLE(WindowHandle);
-	ENTRY_HANDLE(GamepadHandle);
+    ENTRY_HANDLE(WindowHandle);
+    ENTRY_HANDLE(GamepadHandle);
 
-	///
-	constexpr WindowHandle kDefaultWindowHandle = { 0 };
+    ///
+    constexpr WindowHandle kDefaultWindowHandle = { 0 };
 
-	///
-	struct MouseButton
-	{
-		enum Enum
-		{
-			None,
-			Left,
-			Middle,
-			Right,
+    ///
+    struct MouseButton
+    {
+        enum Enum
+        {
+            None,
+            Left,
+            Middle,
+            Right,
 
-			Count
-		};
-	};
+            Count
+        };
+    };
 
-	///
-	struct GamepadAxis
-	{
-		enum Enum
-		{
-			LeftX,
-			LeftY,
-			LeftZ,
-			RightX,
-			RightY,
-			RightZ,
+    ///
+    struct GamepadAxis
+    {
+        enum Enum
+        {
+            LeftX,
+            LeftY,
+            LeftZ,
+            RightX,
+            RightY,
+            RightZ,
 
-			Count
-		};
-	};
+            Count
+        };
+    };
 
-	///
-	struct Modifier
-	{
-		enum Enum
-		{
-			None       = 0,
-			LeftAlt    = 0x01,
-			RightAlt   = 0x02,
-			LeftCtrl   = 0x04,
-			RightCtrl  = 0x08,
-			LeftShift  = 0x10,
-			RightShift = 0x20,
-			LeftMeta   = 0x40,
-			RightMeta  = 0x80,
-		};
-	};
+    ///
+    struct Modifier
+    {
+        enum Enum
+        {
+            None       = 0,
+            LeftAlt    = 0x01,
+            RightAlt   = 0x02,
+            LeftCtrl   = 0x04,
+            RightCtrl  = 0x08,
+            LeftShift  = 0x10,
+            RightShift = 0x20,
+            LeftMeta   = 0x40,
+            RightMeta  = 0x80,
+        };
+    };
 
-	///
-	struct Key
-	{
-		enum Enum
-		{
-			None = 0,
-			Esc,
-			Return,
-			Tab,
-			Space,
-			Backspace,
-			Up,
-			Down,
-			Left,
-			Right,
-			Insert,
-			Delete,
-			Home,
-			End,
-			PageUp,
-			PageDown,
-			Print,
-			Plus,
-			Minus,
-			LeftBracket,
-			RightBracket,
-			Semicolon,
-			Quote,
-			Comma,
-			Period,
-			Slash,
-			Backslash,
-			Tilde,
-			F1,
-			F2,
-			F3,
-			F4,
-			F5,
-			F6,
-			F7,
-			F8,
-			F9,
-			F10,
-			F11,
-			F12,
-			NumPad0,
-			NumPad1,
-			NumPad2,
-			NumPad3,
-			NumPad4,
-			NumPad5,
-			NumPad6,
-			NumPad7,
-			NumPad8,
-			NumPad9,
-			Key0,
-			Key1,
-			Key2,
-			Key3,
-			Key4,
-			Key5,
-			Key6,
-			Key7,
-			Key8,
-			Key9,
-			KeyA,
-			KeyB,
-			KeyC,
-			KeyD,
-			KeyE,
-			KeyF,
-			KeyG,
-			KeyH,
-			KeyI,
-			KeyJ,
-			KeyK,
-			KeyL,
-			KeyM,
-			KeyN,
-			KeyO,
-			KeyP,
-			KeyQ,
-			KeyR,
-			KeyS,
-			KeyT,
-			KeyU,
-			KeyV,
-			KeyW,
-			KeyX,
-			KeyY,
-			KeyZ,
+    ///
+    struct Key
+    {
+        enum Enum
+        {
+            None = 0,
+            Esc,
+            Return,
+            Tab,
+            Space,
+            Backspace,
+            Up,
+            Down,
+            Left,
+            Right,
+            Insert,
+            Delete,
+            Home,
+            End,
+            PageUp,
+            PageDown,
+            Print,
+            Plus,
+            Minus,
+            LeftBracket,
+            RightBracket,
+            Semicolon,
+            Quote,
+            Comma,
+            Period,
+            Slash,
+            Backslash,
+            Tilde,
+            F1,
+            F2,
+            F3,
+            F4,
+            F5,
+            F6,
+            F7,
+            F8,
+            F9,
+            F10,
+            F11,
+            F12,
+            NumPad0,
+            NumPad1,
+            NumPad2,
+            NumPad3,
+            NumPad4,
+            NumPad5,
+            NumPad6,
+            NumPad7,
+            NumPad8,
+            NumPad9,
+            Key0,
+            Key1,
+            Key2,
+            Key3,
+            Key4,
+            Key5,
+            Key6,
+            Key7,
+            Key8,
+            Key9,
+            KeyA,
+            KeyB,
+            KeyC,
+            KeyD,
+            KeyE,
+            KeyF,
+            KeyG,
+            KeyH,
+            KeyI,
+            KeyJ,
+            KeyK,
+            KeyL,
+            KeyM,
+            KeyN,
+            KeyO,
+            KeyP,
+            KeyQ,
+            KeyR,
+            KeyS,
+            KeyT,
+            KeyU,
+            KeyV,
+            KeyW,
+            KeyX,
+            KeyY,
+            KeyZ,
 
-			GamepadA,
-			GamepadB,
-			GamepadX,
-			GamepadY,
-			GamepadThumbL,
-			GamepadThumbR,
-			GamepadShoulderL,
-			GamepadShoulderR,
-			GamepadUp,
-			GamepadDown,
-			GamepadLeft,
-			GamepadRight,
-			GamepadBack,
-			GamepadStart,
-			GamepadGuide,
+            GamepadA,
+            GamepadB,
+            GamepadX,
+            GamepadY,
+            GamepadThumbL,
+            GamepadThumbR,
+            GamepadShoulderL,
+            GamepadShoulderR,
+            GamepadUp,
+            GamepadDown,
+            GamepadLeft,
+            GamepadRight,
+            GamepadBack,
+            GamepadStart,
+            GamepadGuide,
 
-			Count
-		};
-	};
+            Count
+        };
+    };
 
-	///
-	struct Suspend
-	{
-		enum Enum
-		{
-			WillSuspend,
-			DidSuspend,
-			WillResume,
-			DidResume,
+    ///
+    struct Suspend
+    {
+        enum Enum
+        {
+            WillSuspend,
+            DidSuspend,
+            WillResume,
+            DidResume,
 
-			Count
-		};
-	};
+            Count
+        };
+    };
 
-	///
-	const char* getName(Key::Enum _key);
+    ///
+    const char* getName(Key::Enum _key);
 
-	///
-	struct MouseState
-	{
-		MouseState()
-			: m_mx(0)
-			, m_my(0)
-			, m_mz(0)
-		{
-			for (uint32_t ii = 0; ii < entry::MouseButton::Count; ++ii)
-			{
-				m_buttons[ii] = entry::MouseButton::None;
-			}
-		}
+    ///
+    struct MouseState
+    {
+        MouseState()
+                : m_mx(0)
+                , m_my(0)
+                , m_mz(0)
+        {
+            for (uint32_t ii = 0; ii < entry::MouseButton::Count; ++ii)
+            {
+                m_buttons[ii] = entry::MouseButton::None;
+            }
+        }
 
-		int32_t m_mx;
-		int32_t m_my;
-		int32_t m_mz;
-		uint8_t m_buttons[entry::MouseButton::Count];
-	};
+        int32_t m_mx;
+        int32_t m_my;
+        int32_t m_mz;
+        uint8_t m_buttons[entry::MouseButton::Count];
+    };
 
-	///
-	struct GamepadState
-	{
-		GamepadState()
-		{
-			bx::memSet(m_axis, 0, sizeof(m_axis) );
-		}
+    ///
+    struct GamepadState
+    {
+        GamepadState()
+        {
+            bx::memSet(m_axis, 0, sizeof(m_axis) );
+        }
 
-		int32_t m_axis[entry::GamepadAxis::Count];
-	};
+        int32_t m_axis[entry::GamepadAxis::Count];
+    };
 
-	///
-	bool processEvents(uint32_t& _width, uint32_t& _height, uint32_t& _debug, uint32_t& _reset, MouseState* _mouse = NULL);
+    ///
+    bool processEvents(uint32_t& _width, uint32_t& _height, uint32_t& _debug, uint32_t& _reset, MouseState* _mouse = NULL);
 
-	///
-	bx::FileReaderI* getFileReader();
+    ///
+    bx::FileReaderI* getFileReader();
 
-	///
-	bx::FileWriterI* getFileWriter();
+    ///
+    bx::FileWriterI* getFileWriter();
 
-	///
-	bx::AllocatorI*  getAllocator();
+    void setFileReader(bx::FileReaderI* In);
 
-	///
-	WindowHandle createWindow(int32_t _x, int32_t _y, uint32_t _width, uint32_t _height, uint32_t _flags = ENTRY_WINDOW_FLAG_NONE, const char* _title = "");
+    ///
+    void setFileWriter(bx::FileWriterI* In);
 
-	///
-	void destroyWindow(WindowHandle _handle);
+    ///
+    bx::AllocatorI*  getAllocator();
 
-	///
-	void setWindowPos(WindowHandle _handle, int32_t _x, int32_t _y);
+    ///
+    WindowHandle createWindow(int32_t _x, int32_t _y, uint32_t _width, uint32_t _height, uint32_t _flags = ENTRY_WINDOW_FLAG_NONE, const char* _title = "");
 
-	///
-	void setWindowSize(WindowHandle _handle, uint32_t _width, uint32_t _height);
+    ///
+    void destroyWindow(WindowHandle _handle);
 
-	///
-	void setWindowTitle(WindowHandle _handle, const char* _title);
+    ///
+    void setWindowPos(WindowHandle _handle, int32_t _x, int32_t _y);
 
-	///
-	void setWindowFlags(WindowHandle _handle, uint32_t _flags, bool _enabled);
+    ///
+    void setWindowSize(WindowHandle _handle, uint32_t _width, uint32_t _height);
 
-	///
-	void toggleFullscreen(WindowHandle _handle);
+    ///
+    void setWindowTitle(WindowHandle _handle, const char* _title);
 
-	///
-	void setMouseLock(WindowHandle _handle, bool _lock);
+    ///
+    void setWindowFlags(WindowHandle _handle, uint32_t _flags, bool _enabled);
 
-	///
-	void* getNativeWindowHandle(WindowHandle _handle);
+    ///
+    void toggleFullscreen(WindowHandle _handle);
 
-	///
-	void* getNativeDisplayHandle();
+    ///
+    void setMouseLock(WindowHandle _handle, bool _lock);
 
-	///
-	void setCurrentDir(const char* _dir);
+    ///
+    void* getNativeWindowHandle(WindowHandle _handle);
 
-	///
-	struct WindowState
-	{
-		WindowState()
-			: m_width(0)
-			, m_height(0)
-			, m_nwh(NULL)
-		{
-			m_handle.idx = UINT16_MAX;
-		}
+    ///
+    void* getNativeDisplayHandle();
 
-		WindowHandle m_handle;
-		uint32_t     m_width;
-		uint32_t     m_height;
-		MouseState   m_mouse;
-		void*        m_nwh;
-		bx::FilePath m_dropFile;
-	};
+    ///
+    void setCurrentDir(const char* _dir);
 
-	///
-	bool processWindowEvents(WindowState& _state, uint32_t& _debug, uint32_t& _reset);
+    ///
+    struct WindowState
+    {
+        WindowState()
+                : m_width(0)
+                , m_height(0)
+                , m_nwh(NULL)
+        {
+            m_handle.idx = UINT16_MAX;
+        }
 
-	///
-	class BX_NO_VTABLE AppI
-	{
-	public:
-		///
-		AppI(const char* _name, const char* _description, const char* _url = "https://bkaradzic.github.io/bgfx/index.html");
+        WindowHandle m_handle;
+        uint32_t     m_width;
+        uint32_t     m_height;
+        MouseState   m_mouse;
+        void*        m_nwh;
+        bx::FilePath m_dropFile;
+    };
 
-		///
-		virtual ~AppI() = 0;
+    ///
+    bool processWindowEvents(WindowState& _state, uint32_t& _debug, uint32_t& _reset);
 
-		///
-		virtual void init(int32_t _argc, const char* const* _argv, uint32_t _width, uint32_t _height) = 0;
+    ///
+    class BX_NO_VTABLE AppI
+    {
+    public:
+        ///
+        AppI(const char* _name, const char* _description, const char* _url = "https://bkaradzic.github.io/bgfx/index.html");
 
-		///
-		virtual int  shutdown() = 0;
+        ///
+        virtual ~AppI() = 0;
 
-		///
-		virtual bool update() = 0;
+        ///
+        virtual void init(int32_t _argc, const char* const* _argv, uint32_t _width, uint32_t _height) = 0;
 
-		///
-		const char* getName() const;
+        ///
+        virtual int  shutdown() = 0;
 
-		///
-		const char* getDescription() const;
+        ///
+        virtual bool update() = 0;
 
-		///
-		const char* getUrl() const;
+        ///
+        const char* getName() const;
 
-		///
-		AppI* getNext();
+        ///
+        const char* getDescription() const;
 
-	private:
-		BX_ALIGN_DECL(16, uintptr_t) m_internal[4];
-	};
+        ///
+        const char* getUrl() const;
 
-	///
-	AppI* getFirstApp();
+        ///
+        AppI* getNext();
 
-	///
-	uint32_t getNumApps();
+    private:
+        BX_ALIGN_DECL(16, uintptr_t) m_internal[4];
+    };
 
-	///
-	int runApp(AppI* _app, int _argc, const char* const* _argv);
+    ///
+    AppI* getFirstApp();
+
+    ///
+    uint32_t getNumApps();
+
+    ///
+    int runApp(AppI* _app, int _argc, const char* const* _argv);
 
 } // namespace entry
 
